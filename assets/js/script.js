1c31b366b3d46431a04c0a971996fffb1bd78fb6
Yerko Cortes Baeza <yerkodigo@gmail.com>
2021-07-08 21:02:40 -0400

    Exercises 2 changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,28 +64,3 @@ const validar = (nombre,asunto,mensaje) => {
 
 };
 
-
-
-
-
-
-
-// Ejercicio 2 'Selector de colores'
-
-//let btn1 = document.getElementById('btn-1'); 
-//let btn2 = document.getElementById('btn-2'); 
-//let btn3 = document.getElementById('btn-3'); 
-//let btn4 = document.getElementById('btn-4'); 
-//let btn5 = document.getElementById('btn-5'); 
-//let btn6 = document.getElementById('btn-6'); 
-
-//let colorBox = document.getElementById('caja');
-
-//btn1.addEventListener('click', function() {
-//})
-
-//let color = document.getElementsByTagName('button');
-
-//color.addEventListener('click',function(){
-//console.log('bien hecho');
-//})
